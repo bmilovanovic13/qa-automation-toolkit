c939c6b867c4167d3d5361a2020273eca3959c1d
Branko Milovanovic <b.milovanovic@oaza.rs>
2026-06-10 20:18:08 +0200

    Add JWT browser authentication support for hybrid tests

---
+++ test-automation-java/src/test/java/com/branko/ui/utils/AuthenticationHelper.java
@@ -0,0 +1,25 @@
+package com.branko.ui.utils;
+
+import com.branko.shared.AllureUtils;
+import com.branko.shared.Config;
+import com.branko.ui.driver.DriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class AuthenticationHelper {
+
+    public static void loginWithToken(String token) {
+        AllureUtils.step("Authenticate browser using JWT token", () -> {
+            WebDriver driver = DriverManager.getDriver();
+
+            driver.get(
+                    Config.get("baseUrlPracticeTesting")
+            );
+
+            ((JavascriptExecutor) driver)
+                    .executeScript("window.localStorage.setItem('auth-token', arguments[0]);", token);
+
+            driver.navigate().refresh();
+        });
+    }
+}
