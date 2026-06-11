080d956578fed53c1ace54245d8c1729b8faa562
Branko Milovanovic <b.milovanovic@oaza.rs>
2026-06-11 16:25:18 +0200

    Implement hybrid API/UI order flow test

---
+++ test-automation-java/src/test/java/com/branko/hybrid/HybridOrderFlowTest.java
@@ -0,0 +1,39 @@
+package com.branko.hybrid;
+
+import com.branko.api.login.LoginClient;
+import com.branko.api.products.ProductData;
+import com.branko.api.products.ProductsClient;
+import com.branko.ui.base.BaseTest;
+import com.branko.ui.pom.practicesoftwaretesting.ProductsPage;
+import com.branko.ui.utils.AuthenticationHelper;
+import org.testng.annotations.Test;
+
+public class HybridOrderFlowTest extends BaseTest {
+    private final LoginClient loginClient = new LoginClient();
+    private final ProductsClient productsClient = new ProductsClient();
+    @Test(groups = {"hybrid", "smoke-hybrid"}, description = "Verify order can be completed successfully using API authentication and UI checkout flow")
+    public void shouldCompleteOrderUsingHybridApiUiFlow(){
+        String token = loginClient.loginAsCustomerAndGetToken();
+
+        ProductData productInfo = productsClient.getAvailableProduct();
+
+        String productName = productInfo.getName();
+        AuthenticationHelper.loginWithToken(token);
+        ProductsPage.open()
+                .selectProduct(productName)
+                .verifyProductDetailsPage(productName)
+                .addProductToCart()
+                .goToCart()
+                .verifyProductInCart(productName)
+                .proceedToSignInPage()
+                .verifyUserIsAlreadySignedIn()
+                .proceedToBillingPage()
+                .verifyBillingPageIsDisplayed()
+                .fillBillingDetails("Serbia","11060","13","Test street","Belgrade","Test State")
+                .proceedToPaymentPage()
+                .verifyPaymentPageIsDisplayed()
+                .payWith("Cash on Delivery")
+                .verifyPaymentIsSuccessful();
+
+    }
+}
